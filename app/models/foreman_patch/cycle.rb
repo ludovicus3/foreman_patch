fe1e4bbae2461327ae0bde805fa16e78b2495cd0
Jason Galens <bogey.jlg@gmail.com>
2021-02-25 14:52:23 -0500

    Fleshed out creation of cycles and windows

--- app/models/foreman_patch/cycle.rb
+++ app/models/foreman_patch/cycle.rb
@@ -1,10 +1,25 @@
 module ForemanPatch
   class Cycle < ::ApplicationRecord
 
+    before_create :execute_cycle_plan, if: :cycle_plan_id?
+
     belongs_to :cycle_plan, class_name: 'ForemanPatch::CyclePlan'
+    delegate :name, to: :cycle_plan
 
     has_many :windows, class_name: 'ForemanPatch::Window', foreign_key: :cycle_id, inverse_of: :cycle
     has_many :tasks, through: :windows
+
+    scoped_search on: :name, complete_value: true
+    scoped_search on: :start_date, complete_value: false
+
+    private
+
+    def execute_cycle_plan
+      cycle_plan.window_plans.each do |window_plan|
+        windows.build(window_plan: window_plan)
+      end
+      self
+    end
 
   end
 end
--- app/models/foreman_patch/window.rb
+++ app/models/foreman_patch/window.rb
@@ -1,5 +1,7 @@
 module ForemanPatch
   class Window < ::ApplicationRecord
+    before_create :execute_window_plan, if: :window_plan_id?
+
     belongs_to :window_plan, class_name: 'ForemanPatch::WindowPlan'
 
     belongs_to :cycle, class_name: 'ForemanPatch::Cycle', inverse_of: :windows
@@ -14,6 +16,24 @@ module ForemanPatch
 
     has_many :window_groups, class_name: 'ForemanPatch::WindowGroups', inverse_of: :window
     has_many :groups, class_name: 'ForemanPatch::Group', through: :window_groups 
+
+    private
+
+    def execute_window_plan
+      throw :abort unless cycle_id?
+
+      self.tap do |window|
+        window.name = window_plan.name if window.name.nil?
+        window.description = window_plan.description if window.description.nil?
+        window.start_at = (cycle.start_date + window_plan.start_day) + window_plan.start_time.seconds_since_midnight.seconds if window.start_at.nil?
+        window.end_by = window.start_at + window_plan.duration if window.end_by.nil?
+
+        window_plan.groups.each do |group|
+          window.groups << group unless window.groups.include? group
+        end
+      end
+    end
+
   end
 end
 
