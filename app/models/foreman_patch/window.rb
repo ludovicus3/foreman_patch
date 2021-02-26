d07a74bbc7fd8ad04c5f72ba0a2fd1e4da71342c
Jason Galens <bogey.jlg@gmail.com>
2021-02-26 09:37:49 -0500

    Added window validators and scoped_searches

--- app/models/foreman_patch/window.rb
+++ app/models/foreman_patch/window.rb
@@ -17,11 +17,22 @@ module ForemanPatch
     has_many :window_groups, class_name: 'ForemanPatch::WindowGroups', inverse_of: :window
     has_many :groups, class_name: 'ForemanPatch::Group', through: :window_groups 
 
+    validates :cycle, presence: true
+    validates :name, presence: true, on: :update
+    validates :start_at, presence: true, on: :update
+    validates :end_by, presence: true, on: :update
+
+    scoped_search on: :name, complete_value: true
+    scoped_search on: :start_at, complete_value: false
+    scoped_search on: :end_by, complete_value: false
+    scoped_search on: :cycle_id, complete_value: false
+    scoped_search relation: :cycle, on:  :name, complete_value: true, rename: 'cycle', only_explicit: true
+    scoped_search on: :window_plan_id, complete_value: false
+    scoped_search relation: :window_plan, on: :name, complete_value: true, rename: 'window_plan', only_explicit: true
+
     private
 
     def execute_window_plan
-      throw :abort unless cycle_id?
-
       self.tap do |window|
         window.name = window_plan.name if window.name.nil?
         window.description = window_plan.description if window.description.nil?
