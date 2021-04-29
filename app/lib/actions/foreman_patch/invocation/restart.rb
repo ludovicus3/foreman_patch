5983297a8f7daf2f22e95956c134ddbc4202595d
Jason Galens <bogey.jlg@gmail.com>
2021-04-29 15:18:51 -0400

    Restart now polls remote execution port

--- app/lib/actions/foreman_patch/invocation/restart.rb
+++ app/lib/actions/foreman_patch/invocation/restart.rb
@@ -3,6 +3,7 @@ module Actions
     module Invocation
       class Restart < Actions::EntryAction
         include Actions::Helpers::WithFeatureAction
+        include Dynflow::Action::Polling
 
         def plan(host)
           action_subject(host)
@@ -13,7 +14,51 @@ module Actions
           end
         end
 
-        def run(event = nil)
+        def done?
+          external_task[:up]
+        end
+
+        def invoke_external_task
+          if exit_status != 0
+            send_failure_notification
+            fail(_('Restart command failed to execute'))
+          end
+
+          sleep(60)
+        
+          schedule_timeout(Setting[:host_max_wait_for_up]) unless Setting[:host_max_wait_for_up]
+          
+          { up: false }
+        end
+
+        def poll_external_task
+          begin
+            socket = TCPSocket.new(host.ip, Setting[:remote_execution_ssh_port])
+            socket.close
+
+            { up: true }
+          rescue => error
+            Rails.logger.debug(error.message)
+            { up: false }
+          end
+        end
+
+        def process_timeout
+          continuous_output.add_output(_('Server did not respond within alloted time after restart.'))
+
+          send_failure_notification
+
+          fail("Timeout exceeded.")
+        end
+
+        def send_failure_notification
+          users = ::User.select { |user| user.receives?(:patch_invocation_failure) }.compact
+
+          begin
+            MailNotification[:patch_invocation_failure].deliver(user: users, host: host, output: feature_output) unless users.blank?
+          rescue => error
+            message = _('Unable to send patch invocation failure: %{error}') % {error: error}
+          end
         end
 
         def rescue_strategy
--- app/models/setting/patching.rb
+++ app/models/setting/patching.rb
@@ -1,6 +1,9 @@
 class Setting::Patching < ::Setting
   def self.default_settings
-    []
+    [
+      self.set('host_max_wait_for_up', N_("Maximum seconds to wait for a host to register as up after patching restart."),
+               600, N_("Max wait for host up"))
+    ]
   end
 
   def self.humanized_category
@@ -8,7 +11,7 @@ class Setting::Patching < ::Setting
   end
 
   def self.load_defaults
-    BLANK_ATTRS.concat %w()
+    BLANK_ATTRS.concat %w(host_max_wait_for_up)
     super
   end
 end
