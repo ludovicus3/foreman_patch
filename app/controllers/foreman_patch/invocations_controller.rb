a0e5f873847f3cd40c3372184eebdd27db27571b
Jason Galens <bogey.jlg@gmail.com>
2021-05-24 13:44:03 -0400

    Created invocations controller

---
+++ app/controllers/foreman_patch/invocations_controller.rb
@@ -0,0 +1,30 @@
+module ForemanPatch
+  class InvocationsController < ApplicationController
+
+    def show
+      @invocation = Invocation.find(params[:id])
+
+      @host = @invocation.host
+
+      @group = @invocation.window_group
+      @window = @group.window
+      @cycle = @window.cycle
+
+      @auto_refresh = @invocation.task.pending?
+      @since = params[:since].to_f if params[:since].present?
+
+      @invocation.phases.each do |phase|
+        @line_sets = phase.live_output
+        @line_sets = @line_sets.drop_while { |o| o['timestamp'].to_f <= @since } if @since
+
+        unless @line_sets.empty?
+          @phase = phase.label.demodulize.underscore
+          break
+        end
+      end
+
+      @line_counter = params[:line_counter].to_i
+    end
+
+  end
+end
--- app/models/foreman_patch/invocation.rb
+++ app/models/foreman_patch/invocation.rb
@@ -16,6 +16,22 @@ module ForemanPatch
       task.state == 'stopped' and task.result == 'success'
     end
 
+    def phases
+      task.main_action.planned_actions unless task.blank?
+    end
+
+    def status
+      HostStatus::ExecutionStatus::ExecutionTaskStatusMapper.new(task).status
+    end
+
+    def status_label
+      HostStatus::ExecutionStatus::ExecutionTaskStatusMapper.new(task).status_label
+    end
+
+    def queued?
+      status == HostStatus::ExecutionStatus::QUEUED
+    end
+
     def to_action_input
       {
         id: id,
