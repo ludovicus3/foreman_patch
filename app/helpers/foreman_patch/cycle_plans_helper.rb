6ba5cf9d0fd8e9e0553a6bf4772ea42c2991f8cb
Jason Galens <bogey.jlg@gmail.com>
2021-06-11 15:40:29 -0400

    Added forgotten helper

--- app/helpers/foreman_patch/cycle_plans_helper.rb
+++ app/helpers/foreman_patch/cycle_plans_helper.rb
@@ -2,12 +2,20 @@ module ForemanPatch
   module CyclePlansHelper
 
     def plan_windows(cycle_plan, day)
-      day = (day - cycle_plan.next_cycle).to_i
+      day = (day - cycle_plan.next_cycle_date).to_i
 
       cycle_plan.window_plans.select do |window|
         window.start_day == day
       end
     end
 
+    def plan_window_actions(window)
+      actions = []
+
+      actions << link_to(_('Edit'), edit_window_plan_path(window))
+      actions << link_to(_('Delete'), hash_for_window_plan_path(id: window), data: { confirm: _('Are you sure?') }, action: :destroy)
+
+      actions
+    end
   end
 end
