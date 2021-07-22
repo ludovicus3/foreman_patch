ed544982ef82302760e15b3e63ef4d31498b3343
Jason Galens <bogey.jlg@gmail.com>
2021-07-22 14:58:57 -0400

    Fixed time zoning in cycle_mailer

--- app/mailers/foreman_patch/cycle_mailer.rb
+++ app/mailers/foreman_patch/cycle_mailer.rb
@@ -3,12 +3,12 @@ module ForemanPatch
     helper ForemanPatch::TicketHelper
 
     def planned(options)
-      user = options[:user]
+      @user = options[:user]
 
       @cycle = options[:cycle]
 
-      set_locale_for(user) do
-        mail(to: user.mail, subject: (_("Patching Cycle Planned")))
+      set_locale_for(@user) do
+        mail(to: @user.mail, subject: (_("Patching Cycle Planned")))
       end
     end
 
