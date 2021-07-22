e1c886536423da325b1aa52c7e76b109204a7738
Jason Galens <bogey.jlg@gmail.com>
2021-07-22 09:27:53 -0400

    Sort windows by start and add end_date to cycle

--- app/models/foreman_patch/cycle.rb
+++ app/models/foreman_patch/cycle.rb
@@ -3,14 +3,16 @@ module ForemanPatch
 
     belongs_to :cycle_plan, class_name: 'ForemanPatch::CyclePlan'
 
-    has_many :windows, class_name: 'ForemanPatch::Window', foreign_key: :cycle_id, inverse_of: :cycle
+    has_many :windows, -> { order(start_at: :asc) }, class_name: 'ForemanPatch::Window', foreign_key: :cycle_id, inverse_of: :cycle
     has_many :tasks, through: :windows
 
     scoped_search on: :name, complete_value: true
     scoped_search on: :start_date, complete_value: false
+    scoped_search on: :end_date, complete_value: false
 
     validates :name, presence: true
     validates :start_date, presence: true
+    validates :end_date, presence: true
 
     before_validation :plan_cycle, if: :cycle_plan_id?
     after_create :plan_windows, if: :cycle_plan_id?
@@ -24,6 +26,7 @@ module ForemanPatch
     def plan_cycle
       self.name = cycle_plan.name if name.nil?
       self.description = cycle_plan.description if description.nil?
+      self.end_date = start_date + cycle_plan.frequency if end_date.nil?
     end
 
     def plan_windows
