a9ce771d789a18363cd4a2cccc8af5150adfa362
Jason Galens <bogey.jlg@gmail.com>
2021-09-14 10:37:12 -0400

    Alias resource

--- app/models/foreman_patch/plan_task_group.rb
+++ app/models/foreman_patch/plan_task_group.rb
@@ -2,6 +2,8 @@ module ForemanPatch
   class PlanTaskGroup < ::ForemanTasks::TaskGroup
     has_one :plan, foreign_key: :task_group_id, dependent: :nullify, inverse_of: :task_group, class_name: 'ForemanPatch::Plan'
 
+    alias_method :resource, :plan
+
     def resource_name
       N_('Plan')
     end
