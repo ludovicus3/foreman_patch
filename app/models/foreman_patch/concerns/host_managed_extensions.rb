5360f14d6b514fd72424fb9044a4bd7e90e8de17
Jason Galens <bogey.jlg@gmail.com>
2021-09-29 15:54:45 -0400

    Fixed host search by round_id

--- app/models/foreman_patch/concerns/host_managed_extensions.rb
+++ app/models/foreman_patch/concerns/host_managed_extensions.rb
@@ -7,7 +7,7 @@ module ForemanPatch
         has_many  :invocations, class_name: 'ForemanPatch::Invocation', foreign_key: :host_id
         has_many  :rounds, class_name: 'ForemanPatch::Round', through: :invocations
 
-        scoped_search relation: :rounds, on: :id, complete_value: false, rename: :patch_group_id
+        scoped_search relation: :rounds, on: :id, complete_value: false, rename: :patch_round_id
       end
 
     end
