e028ff7e17474ffac35b7036b9b83a134225a383
Jason Galens <bogey.jlg@gmail.com>
2022-01-05 13:32:44 -0500

    Resolve hosts for patch window

---
+++ app/lib/actions/foreman_patch/window/resolve_hosts.rb
@@ -0,0 +1,31 @@
+module Actions
+  module ForemanPatch
+    module Window
+      class ResolveHosts < Actions::EntryAction
+
+        def plan(window)
+          input.update serialize_args(window: window)
+
+          sequence do
+            concurrence do
+              window.rounds.each do |round|
+                plan_action(Actions::ForemanPatch::Round::ResolveHosts, round)
+              end
+            end
+
+            plan_action(Actions::ForemanPatch::Window::Publish, window)
+          end
+        end
+
+        def window
+          @window ||= ::ForemanPatch::Window.find(input[:window][:id])
+        end
+
+        def humanized_name
+          _('Resolve Hosts for %s') % window.name
+        end
+
+      end
+    end
+  end
+end
