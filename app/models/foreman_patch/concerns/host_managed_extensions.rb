cb222412c9a2508259db16839d36990709c2f6cb
Jason Galens <bogey.jlg@gmail.com>
2022-01-21 11:02:34 -0500

    Spacing adjust

--- app/models/foreman_patch/concerns/host_managed_extensions.rb
+++ app/models/foreman_patch/concerns/host_managed_extensions.rb
@@ -4,8 +4,8 @@ module ForemanPatch
       extend ActiveSupport::Concern
 
       included do
-        has_many  :invocations, class_name: 'ForemanPatch::Invocation', foreign_key: :host_id, dependent: :delete_all
-        has_many  :rounds, class_name: 'ForemanPatch::Round', through: :invocations
+        has_many :invocations, class_name: 'ForemanPatch::Invocation', foreign_key: :host_id, dependent: :delete_all
+        has_many :rounds, class_name: 'ForemanPatch::Round', through: :invocations
 
         scoped_search relation: :rounds, on: :id, complete_value: false, rename: :patch_round_id
       end
