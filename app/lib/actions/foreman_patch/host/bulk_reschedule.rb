ed21c4f32fa6b1323281b7542bff88eb148732f1
Jason Galens <bogey.jlg@gmail.com>
2022-09-21 14:25:47 -0400

    Ensure order of operations

--- app/lib/actions/foreman_patch/host/bulk_reschedule.rb
+++ app/lib/actions/foreman_patch/host/bulk_reschedule.rb
@@ -11,8 +11,10 @@ module Actions
           windows = ::ForemanPatch::Window.planned.joins(:hosts).where(hosts: { id: hosts }).distinct +
             ::ForemanPatch::Window.planned.where(rounds: rounds).distinct
 
-          plan_action(Actions::BulkAction, Actions::ForemanPatch::Host::Reschedule, hosts)
-          plan_action(Actions::BulkAction, Actions::ForemanPatch::Window::Publish, windows)
+          sequence do
+            plan_action(Actions::BulkAction, Actions::ForemanPatch::Host::Reschedule, hosts)
+            plan_action(Actions::BulkAction, Actions::ForemanPatch::Window::Publish, windows)
+          end
         end
 
       end
