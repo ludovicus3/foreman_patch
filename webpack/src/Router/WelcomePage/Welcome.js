599dc13c5799b431fe13b66afbc51796db36cdf6
Jason <bogey.jlg@gmail.com>
2023-05-17 13:14:07 +0000

    Welcom stub

--- webpack/src/Router/WelcomePage/Welcome.js
+++ webpack/src/Router/WelcomePage/Welcome.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { translate as __ } from 'foremanReact/common/I18n';
+import { Button, Alert } from '@patternfly/react-core';
+import EmptyState from 'foremanReact/components/common/EmptyState/EmptyStatePattern';
 
-const WelcomePage = () => (
-);
+const WelcomePage = () => {
+  const createBtn = (
+    <Button>TODO</Button>
+  );
+
+  const header = "Patching";
+
+  const description = "The welcome page";
+
+  return (
+    <EmptyState
+      icon="add-circle-o"
+      action={createBtn}
+      header={header}
+      description={description}
+      documentation={false}
+    />
+  );
+};
 
 export default WelcomePage;
