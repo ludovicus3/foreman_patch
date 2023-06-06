ac80e2404f9946e4f34aada359f3d18cc9bf3907
Jason <bogey.jlg@gmail.com>
2023-06-06 18:59:09 +0000

    Dropped unused function

--- db/seeds.d/100-assign_features_with_templates.rb
+++ db/seeds.d/100-assign_features_with_templates.rb
@@ -1,16 +1,10 @@
 User.as_anonymous_admin do
-  RemoteExecutionFeature.without_auditing do
-    if Rails.env.test? || Foreman.in_rake?
-      ForemanPatch.register_rex_features
-    end
-
-    JobTemplate.without_auditing do
-      module_template = JobTemplate.find_by(name: 'Power Action - SSH Default')
-      if module_template && !Rails.env.test? && Setting[:remote_execution_sync_templates]
-        module_template.sync_feature('power_action')
-        module_template.organizations << Organization.unscoped.all if module_template.organizations.empty?
-        module_template.locations << Location.unscoped.all if module_template.locations.empty?
-      end
+  JobTemplate.without_auditing do
+    module_template = JobTemplate.find_by(name: 'Power Action - SSH Default')
+    if module_template && !Rails.env.test? && Setting[:remote_execution_sync_templates]
+      module_template.sync_feature('power_action')
+      module_template.organizations << Organization.unscoped.all if module_template.organizations.empty?
+      module_template.locations << Location.unscoped.all if module_template.locations.empty?
     end
   end
 end
